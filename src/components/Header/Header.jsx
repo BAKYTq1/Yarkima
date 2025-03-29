0cfa640d1a0b460eb234b3ea84f1944d94768a6e
privat <privativv@gmail.com>
2025-03-29 19:31:48 +0600

    added header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React from "react";
+import "./Header.scss";
+import Logo from "../../assets/image/logo.png"
+import LogoText from "../../assets/image/logoText.png"
+import { CiSearch } from "react-icons/ci";
+import { FaGripLines } from "react-icons/fa";
 
-function Header() {
+
+
+
+const Header = () => {
   return (
-    <div>
-      header
-    </div>
-  )
-}
+    <header className="header container">
+      <div className="box container">
+        {/* Логотип */}
+        <div className="logo">
+          <img src={Logo} alt="" />
+          <img className="logoText" src={LogoText} alt="" />
+        </div>
 
-export default Header
+        {/* Кнопка "Изучить" */}
+        <button className="dropdown">ИЗУЧИТЬ</button>
+
+        {/* Поиск */}
+        <div className="search">
+          <span className="icon"><CiSearch /></span>
+          <input type="text" placeholder="Поиск" />
+        </div>
+
+        {/* Кнопки */}
+        <div>
+        <button className="login">ВОЙТИ</button>
+        <button className="register">ЗАРЕГИСТРИРОВАТЬСЯ</button>
+        <span className="menu">
+        <FaGripLines /></span>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
