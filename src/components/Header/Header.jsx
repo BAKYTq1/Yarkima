2de2dff0db93c3110deba2d15daa7efd39914fbc
BAKYTq1 <bsainaev@gmail.com>
2025-04-08 17:02:37 +0600

    first

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import "./Header.scss";
+
 import Logo from "../../assets/svg/logo.svg";
 import LogoText from "../../assets/image/logoText.png";
+
 import { CiSearch } from "react-icons/ci";
 import { FaGripLines } from "react-icons/fa";
 import { IoMdClose } from "react-icons/io";
@@ -14,18 +16,18 @@ const Header = () => {
     <>
       <header className="header">
         <div className="box container">
-          {/* Logo */}
+          {/* Логотип */}
           <Link to="/">
             <div className="logo">
               <img src={Logo} alt="logo" />
               <img className="logoText" src={LogoText} alt="logo text" />
             </div>
           </Link>
-          
-          {/* Study button */}
+
+          {/* Кнопка "Изучить" */}
           <button className="dropdown99">ИЗУЧИТЬ 🔻</button>
-          
-          {/* Search */}
+
+          {/* Поиск */}
           <div className="search">
             <span className="icon"><CiSearch /></span>
             <input type="text" placeholder="Поиск" />
@@ -35,11 +37,6 @@ const Header = () => {
           <div>
             <Link to={'/login'}><button className="login">ВОЙТИ</button></Link>
            <Link to={'register'}><button className="register">ЗАРЕГИСТРИРОВАТЬСЯ</button></Link>
-          
-          {/* Buttons */}
-          <div className="auth-buttons">
-            <button className="login">ВОЙТИ</button>
-            <button className="register">ЗАРЕГИСТРИРОВАТЬСЯ</button>
             <span className="menu" onClick={() => setMenuOpen(!menuOpen)}>
               <FaGripLines />
             </span>
@@ -47,7 +44,7 @@ const Header = () => {
         </div>
       </header>
 
-      {/* Dropdown menu */}
+      {/* Выпадающее меню */}
       <div className={`dropdown-menu ${menuOpen ? "open" : ""}`}>
         <button className="close-btn" onClick={() => setMenuOpen(false)}>
           <IoMdClose />
@@ -57,7 +54,7 @@ const Header = () => {
             <li><Link to="/" onClick={() => setMenuOpen(false)}>ГЛАВНАЯ</Link></li>
             <li><Link to="/catalog" onClick={() => setMenuOpen(false)}>КАТАЛОГ</Link></li>
             <li><Link to="/cases" onClick={() => setMenuOpen(false)}>КЕЙСЫ</Link></li>
-            <li><Link to="/FAQ" onClick={() => setMenuOpen(false)}>F.A.Q</Link></li>
+            <li><Link to="FAQ" onClick={() => setMenuOpen(false)}>F.A.Q</Link></li>
             <li><Link to="/partners" onClick={() => setMenuOpen(false)}>ПАРТНЕРАМ</Link></li>
           </ul>
         </nav>
