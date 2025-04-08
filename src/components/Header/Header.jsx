12c0dc8f1b03adab9f167324cbbe706eba5ff6e3
privat <privativv@gmail.com>
2025-04-08 16:32:22 +0600

    add faq

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,7 @@
 import React, { useState } from "react";
 import "./Header.scss";
-
 import Logo from "../../assets/svg/logo.svg";
 import LogoText from "../../assets/image/logoText.png";
-
 import { CiSearch } from "react-icons/ci";
 import { FaGripLines } from "react-icons/fa";
 import { IoMdClose } from "react-icons/io";
@@ -13,37 +11,28 @@ const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <header className="header">
-      <div className="box ">
-        {/* Логотип */}
-        <Link to={'/'}>
-        <div className="logo">
-          <img src={Logo} alt="" />
-          <img className="logoText" src={LogoText} alt="" />
-        </div>
-        </Link>
     <>
       <header className="header">
         <div className="box container">
-          {/* Логотип */}
+          {/* Logo */}
           <Link to="/">
             <div className="logo">
               <img src={Logo} alt="logo" />
               <img className="logoText" src={LogoText} alt="logo text" />
             </div>
           </Link>
-
-          {/* Кнопка "Изучить" */}
+          
+          {/* Study button */}
           <button className="dropdown99">ИЗУЧИТЬ 🔻</button>
-
-          {/* Поиск */}
+          
+          {/* Search */}
           <div className="search">
             <span className="icon"><CiSearch /></span>
             <input type="text" placeholder="Поиск" />
           </div>
-
-          {/* Кнопки */}
-          <div>
+          
+          {/* Buttons */}
+          <div className="auth-buttons">
             <button className="login">ВОЙТИ</button>
             <button className="register">ЗАРЕГИСТРИРОВАТЬСЯ</button>
             <span className="menu" onClick={() => setMenuOpen(!menuOpen)}>
@@ -53,7 +42,7 @@ const Header = () => {
         </div>
       </header>
 
-      {/* Выпадающее меню */}
+      {/* Dropdown menu */}
       <div className={`dropdown-menu ${menuOpen ? "open" : ""}`}>
         <button className="close-btn" onClick={() => setMenuOpen(false)}>
           <IoMdClose />
@@ -63,7 +52,7 @@ const Header = () => {
             <li><Link to="/" onClick={() => setMenuOpen(false)}>ГЛАВНАЯ</Link></li>
             <li><Link to="/catalog" onClick={() => setMenuOpen(false)}>КАТАЛОГ</Link></li>
             <li><Link to="/cases" onClick={() => setMenuOpen(false)}>КЕЙСЫ</Link></li>
-            <li><Link to="FAQ" onClick={() => setMenuOpen(false)}>F.A.Q</Link></li>
+            <li><Link to="/FAQ" onClick={() => setMenuOpen(false)}>F.A.Q</Link></li>
             <li><Link to="/partners" onClick={() => setMenuOpen(false)}>ПАРТНЕРАМ</Link></li>
           </ul>
         </nav>
