a1142812bc6feb907b41c489a1df05ca38c36359
alfi <alfiaskard@gmail.com>
2025-04-09 18:06:12 +0300

    alfi's code

---
+++ answers/answer/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+class TimeAbstract(models.Model):
+    
+    created_at = models.DateField('created', auto_now_add=True)
+    updated_at = models.DateField('updated', auto_now=True)
+    
+    class Meta:
+        abstract = True
+        
+class Answer(models.Model):
+    
+    class Meta:
+        verbose_name_plural = 'отзывы'
+        verbose_name = 'отзыв'
+        
+    answer = models.CharField('отзыв', max_length=500)
+    
+    
+
+# Create your models here.
---
+++ answers/api/serializers.py
@@ -0,0 +1,9 @@
+from answer.models import Answer
+from rest_framework import serializers
+
+
+class AnswerSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Answer
+        fields = ('__all__')
